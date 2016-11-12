c9b3a26227a4452c768623c31903ac84f3920c1b
musyoku <musyoku@users.noreply.github.com>
2016-11-12 22:20:46 +0900

    j

--- train_killmebaby/plot.py
+++ train_killmebaby/plot.py
@@ -12,7 +12,6 @@ def plot(filename="gen"):
 		pass
 
 	x_negative = ddgm.generate_x(100, test=True, as_numpy=True)
-	print x_negative.shape
 	# x_negative = (x_negative + 1) / 2
 	visualizer.tile_rgb_images(x_negative.transpose(0, 2, 3, 1), dir=args.plot_dir, filename=filename)
 
